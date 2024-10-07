aac76df1aadb229418b33cb0a9db954421834e19
Flavius Ilinoiu <108665687+flaviusilinoiu@users.noreply.github.com>
2024-10-07 16:02:35 +0300

    Invalidate dns ipv6 queries.

--- mitmproxy/addons/cyberhaven/cyberhaven.py
+++ mitmproxy/addons/cyberhaven/cyberhaven.py
@@ -1,6 +1,10 @@
-class MainCyberhaven:
-    def __init__(self):
-        pass
+from mitmproxy import dns
 
+class MainCyberhaven:
     def response(self, flow):
-        flow.response.headers["CH"] = "processed"
+        flow.response.headers["ch"] = "processed"
+
+    def dns_response(self, flow: dns.DNSFlow):
+        if flow.request.question.type == 28:
+            flow.response.answers = []
+            
